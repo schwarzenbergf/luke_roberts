4a0a6e0ed34a3e37256e60dd1e1f00b5c6405fc0
Frank Schwarzenberg <52888262+schwarzenbergf@users.noreply.github.com>
2023-03-09 17:04:50 +0000

    modified:   custom_components/luke_roberts/__init__.py 	modified:   custom_components/luke_roberts/api.py 	modified:   custom_components/luke_roberts/binary_sensor.py 	modified:   custom_components/luke_roberts/config_flow.py 	modified:   custom_components/luke_roberts/coordinator.py 	modified:   custom_components/luke_roberts/entity.py 	modified:   custom_components/luke_roberts/sensor.py 	modified:   custom_components/luke_roberts/switch.py

--- custom_components/luke_roberts/api.py
+++ custom_components/luke_roberts/api.py
@@ -8,23 +8,19 @@ import aiohttp
 import async_timeout
 
 
-class IntegrationBlueprintApiClientError(Exception):
+class LukeRobertsApiClientError(Exception):
     """Exception to indicate a general API error."""
 
 
-class IntegrationBlueprintApiClientCommunicationError(
-    IntegrationBlueprintApiClientError
-):
+class LukeRobertsApiClientCommunicationError(LukeRobertsApiClientError):
     """Exception to indicate a communication error."""
 
 
-class IntegrationBlueprintApiClientAuthenticationError(
-    IntegrationBlueprintApiClientError
-):
+class LukeRobertsApiClientAuthenticationError(LukeRobertsApiClientError):
     """Exception to indicate an authentication error."""
 
 
-class IntegrationBlueprintApiClient:
+class LukeRobertsApiClient:
     """Sample API Client."""
 
     def __init__(
@@ -70,21 +66,21 @@ class IntegrationBlueprintApiClient:
                     json=data,
                 )
                 if response.status in (401, 403):
-                    raise IntegrationBlueprintApiClientAuthenticationError(
+                    raise LukeRobertsApiClientAuthenticationError(
                         "Invalid credentials",
                     )
                 response.raise_for_status()
                 return await response.json()
 
         except asyncio.TimeoutError as exception:
-            raise IntegrationBlueprintApiClientCommunicationError(
+            raise LukeRobertsApiClientCommunicationError(
                 "Timeout error fetching information",
             ) from exception
         except (aiohttp.ClientError, socket.gaierror) as exception:
-            raise IntegrationBlueprintApiClientCommunicationError(
+            raise LukeRobertsApiClientCommunicationError(
                 "Error fetching information",
             ) from exception
         except Exception as exception:  # pylint: disable=broad-except
-            raise IntegrationBlueprintApiClientError(
+            raise LukeRobertsApiClientError(
                 "Something really wrong happened!"
             ) from exception
